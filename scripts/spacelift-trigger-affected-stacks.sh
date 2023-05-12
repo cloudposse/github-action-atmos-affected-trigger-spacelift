32f024dce618c82f374f6cb2334e9151d1070834
Matt Calhoun <matt@mattcalhoun.com>
2023-05-12 13:09:54 -0400

    add timing

--- scripts/spacelift-trigger-affected-stacks.sh
+++ scripts/spacelift-trigger-affected-stacks.sh
@@ -10,12 +10,24 @@ fi
 declare -A error_stacks
 success_stacks=()
 
+total_duration=0
+
 # Use jq to extract the spacelift_stack values and iterate through them
 for spacelift_stack in $(echo "$affected_stacks" | jq -r '.[].spacelift_stack'); do
+  start_time=$(date +%s%N)  # Get the start time in nanoseconds
+
   # Run the spacectl command, capture the error message, and store the exit status
-  echo "running spacectl stack $spacectl_command --id \"$spacelift_stack\" --sha \"$TRIGGERING_SHA\""
+  echo "Running spacectl stack $spacectl_command --id \"$spacelift_stack\" --sha \"$TRIGGERING_SHA\""
   error_message=$(spacectl stack $spacectl_command --id "$spacelift_stack" --sha "$TRIGGERING_SHA" 2>&1)
   exit_status=$?
+
+  end_time=$(date +%s%N)  # Get the end time in nanoseconds
+  duration=$((end_time - start_time))  # Calculate the duration in nanoseconds
+
+  # Convert the duration to seconds with milliseconds precision
+  seconds=$((duration / 1000000000))
+  milliseconds=$(((duration % 1000000000) / 1000000))
+  duration_seconds="$seconds.$(printf "%03d" "$milliseconds")"
 
   if [ $exit_status -ne 0 ]; then
     # If the command failed, add the spacelift_stack and error message to the error_stacks associative array
@@ -24,7 +36,16 @@ for spacelift_stack in $(echo "$affected_stacks" | jq -r '.[].spacelift_stack');
     # If the command succeeded, add the spacelift_stack to the success_stacks array
     success_stacks+=("$spacelift_stack")
   fi
+
+  echo "Duration: $duration_seconds seconds"
+  echo
+  total_duration=$((total_duration + duration))
 done
+
+# Convert the total duration to seconds with milliseconds precision
+total_seconds=$((total_duration / 1000000000))
+total_milliseconds=$(((total_duration % 1000000000) / 1000000))
+total_duration_seconds="$total_seconds.$(printf "%03d" "$total_milliseconds")"
 
 # Output the list of successful stacks, if any
 if [ ${#success_stacks[@]} -gt 0 ]; then
@@ -35,6 +56,8 @@ if [ ${#success_stacks[@]} -gt 0 ]; then
 else
   echo "No stacks triggered successfully."
 fi
+
+echo "Total Duration: $total_duration_seconds seconds"
 
 if [ ${#error_stacks[@]} -eq 0 ]; then
   # Exit with 0 if there are no errors
