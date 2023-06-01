833b30290747e8465245ad2de78acf83fb515ff7
Matt Calhoun <matt@mattcalhoun.com>
2023-06-01 15:36:16 -0400

    update action for comment-based flow

---
+++ scripts/spacelift-generate-pr-comment-body.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+spacectl_command="preview"
+
+if [ "$1" = "true" ]; then
+  spacectl_command="deploy"
+fi
+
+body=""
+
+# Use jq to extract the spacelift_stack values and iterate through them
+for spacelift_stack in $(jq -r '.[].spacelift_stack' < "affected-stacks.json" | grep -v null); do
+  body="$body\n/spacelift $spacectl_command $spacelift_stack"
+done
+
+echo -e "$body"
