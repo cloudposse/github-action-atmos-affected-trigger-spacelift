a9f015ca759634fb11769cae0bcfea9d0c603983
Matt Calhoun <matt@mattcalhoun.com>
2023-06-01 17:19:09 -0400

    add newline

--- scripts/spacelift-generate-pr-comment-body.sh
+++ scripts/spacelift-generate-pr-comment-body.sh
@@ -8,5 +8,5 @@ fi
 
 # Use jq to extract the spacelift_stack values and iterate through them
 for spacelift_stack in $(jq -r '.[].spacelift_stack' < "affected-stacks.json" | grep -v null); do
-  printf "/spacelift %s %s" "$spacectl_command" "$spacelift_stack" >> "comment-body.txt"
+  printf "/spacelift %s %s" "$spacectl_command" "$spacelift_stack\n" >> "comment-body.txt"
 done
