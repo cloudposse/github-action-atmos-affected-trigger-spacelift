d102ba3671e753df743665a74700a93efc63eee3
Matt Calhoun <matt@mattcalhoun.com>
2023-06-01 17:09:25 -0400

    use file for passing string

--- scripts/spacelift-generate-pr-comment-body.sh
+++ scripts/spacelift-generate-pr-comment-body.sh
@@ -6,11 +6,7 @@ if [ "$1" = "true" ]; then
   spacectl_command="deploy"
 fi
 
-body=""
-
 # Use jq to extract the spacelift_stack values and iterate through them
 for spacelift_stack in $(jq -r '.[].spacelift_stack' < "affected-stacks.json" | grep -v null); do
-  body="$body\n/spacelift $spacectl_command $spacelift_stack"
+  printf "/spacelift %s %s" "$spacectl_command" "$spacelift_stack" >> "comment-body.txt"
 done
-
-printf "%s\n" "$body"
