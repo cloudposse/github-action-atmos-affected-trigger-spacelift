57c3dc0056e9fb8ceaf5c9395961408fe3d38f35
Maksym Vlasov <MaxymVlasov@users.noreply.github.com>
2023-06-13 20:36:14 +0300

    feat: Hide spacelift runs

---
+++ scripts/hide-comment-body.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo '<details><summary>Spacelift runs</summary>' > hidden-comment-body.txt
+cat comment-body.txt >> hidden-comment-body.txt
+echo '</details>' >> hidden-comment-body.txt
