2de27733d9a909f5321234ed0c9921b878d1f933
Maksym Vlasov <MaxymVlasov@users.noreply.github.com>
2023-06-13 20:54:20 +0300

    f

--- scripts/hide-comment-body.sh
+++ scripts/hide-comment-body.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-echo '<details><summary>Spacelift runs</summary>' > hidden-comment-body.txt
+echo '<details><summary>Spacelift runs</summary>
+```
+' > hidden-comment-body.txt
+
 cat comment-body.txt >> hidden-comment-body.txt
-echo '</details>' >> hidden-comment-body.txt
+
+echo '
+```
+</details>' >> hidden-comment-body.txt
