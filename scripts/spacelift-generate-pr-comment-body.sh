1fb52b3a4c1fed5c41dff5fc989a7e0a5cb0bad3
Matt Calhoun <matt@cloudposse.com>
2023-06-21 19:20:26 -0400

    wrap comments in details block (#20)
    * wrap comments in details block
    * update body logic

--- scripts/spacelift-generate-pr-comment-body.sh
+++ scripts/spacelift-generate-pr-comment-body.sh
@@ -7,6 +7,12 @@ if [ "$1" = "true" ]; then
 fi
 
 # Use jq to extract the spacelift_stack values and iterate through them
+stack_count=0
 for spacelift_stack in $(jq -r '.[].spacelift_stack' < "affected-stacks.json" | grep -v null); do
   printf "/spacelift %s %s\n" "$spacectl_command" "$spacelift_stack" >> "comment-body.txt"
+  stack_count=$((stack_count+1))
 done
+
+# Wrap the contents in a collapsible details block
+sed -i "1 i\<details><summary>Spacelift Triggered Stacks ($stack_count)</summary>" comment-body.txt
+printf "</details>\n" >> "comment-body.txt"
