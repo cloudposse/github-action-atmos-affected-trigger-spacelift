a691bda415f0a46b746214941fc1f90020e2fa12
Matt Calhoun <matt@cloudposse.com>
2023-08-04 16:03:52 -0400

    add fix (#26)

--- scripts/spacelift-generate-pr-comment-body.sh
+++ scripts/spacelift-generate-pr-comment-body.sh
@@ -9,7 +9,7 @@ fi
 # Use jq to extract the spacelift_stack values and iterate through them
 stack_count=0
 for spacelift_stack in $(jq -r '.[].spacelift_stack' < "affected-stacks.json" | grep -v null); do
-  printf "/spacelift %s %s\n" "$spacectl_command" "$spacelift_stack" >> "comment-body.txt"
+  printf "/spacelift %s %s\n" "$spacelift_stack" "$spacectl_command" >> "comment-body.txt"
   stack_count=$((stack_count+1))
 done
 
